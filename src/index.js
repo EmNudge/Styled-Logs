eace7ab2d78485b703309e727599dd85ae341b5c
EmNudge <emnudge@gmail.com>
2019-07-02 14:38:17 -0400

    Fixed weird cut offs. Moved to less elegant, but more correct algorithm.
    Added an alias array property so that <br /> and custom tags could be implemented. Self closing tags are easier now. A lot of it uses regex.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import "./styles.css";
 import StyledLog from "./StyledLog";
 
 new StyledLog().html`
-  hello my
-  <spacer/><div class="adj">good</div><spacer/>
-  <br>
-  and
-  <div class="my-adj">special</div>
-  friend!
+  This is Styled Logs!
+  <br />
+  this is the package of:
+  <br />
+  <br />
+  <div class="name">styled-logs</div>
+  <div class="version">v0.1.0</div>
 `.css`
-  adj { color: yellow; }
-  my-adj { background: yellow; }
+  .name { 
+    color: white;
+    background: linear-gradient(#555, #333);
+    padding: 2px 6px;
+    border-radius: 4px 0 0 4px;
+  }
+  .version {
+    color: white;
+    background: linear-gradient(#E86, #C64);
+    padding: 2px 6px;
+    border-radius: 0 4px 4px 0;
+  }
 `.log();
+
+const platform = navigator.platform.toUpperCase();
+const isMac = platform.includes("MAC");
+const isLinux = platform.includes("LINUX");
+
+let shortcut = "Ctrl + Shift + J";
+if (isMac) shortcut = "Cmd + Option + J";
+if (isLinux) shortcut = "Ctrl + Shift + I";
+document.querySelector(".shortcut").textContent = `(${shortcut})`;
