7900c7bc589ea34595060e788dcd22726904710a
EmNudge <emnudge@gmail.com>
2021-01-26 22:58:01 -0500

    removed examples from repo

--- examples/logs/Basic.js
+++
@@ -1,9 +0,0 @@
-import StyledLog from "../../build/StyledLogs.min.js";
-
-new StyledLog().html`
-  This is a <div class="styled">styled console log</div>
-`.css`
-  .styled {
-    color: #6182C1;
-  }
-`.log();
